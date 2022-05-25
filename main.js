11ca55e8e1e945b036c601c10ca0c7f875db0983
adrianemaciel <adrianemaciel.developer@gmail.com>
2022-05-25 17:06:35 -0300

    adicionando funcao no botao flutuante

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
-  navigation.classList.add('scroll')
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove( 'scroll')
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
